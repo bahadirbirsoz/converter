7aa7045c86fd10951c87014df0672841c763884d
Bahadır Birsöz <bahadirbirsoz@gmail.com>
2019-06-08 12:35:23 +0300

    Added toJson Method

--- src/Writer/Dart.php
+++ src/Writer/Dart.php
@@ -16,9 +16,9 @@ class Dart extends Writer
     }
 
 
-
-    public function getStaticTypeOf(Argument $argument){
-        switch (true){
+    public function getStaticTypeOf(Argument $argument)
+    {
+        switch (true) {
             case $argument->is(ArgumentType::TYPE_INTEGER):
                 return "int";
             case $argument->is(ArgumentType::TYPE_DECIMAL):
@@ -45,70 +45,119 @@ class Dart extends Writer
         }
     }
 
-    public function write(){
+    public function write()
+    {
         ob_start();
         $writer = $this;
         $parser = $this->parser;
         include $this->getClassTemplatePath();
         $content = ob_get_contents();
-        var_dump($content);die;
+        var_dump($content);
+        die;
     }
 
 
-    protected function getClassTemplatePath(){
+    protected function getClassTemplatePath()
+    {
         return 'lib/views/dart/struct.phtml';
     }
 
-    protected function getTargetArgumentName(Argument $argument){
+    protected function getTargetArgumentName(Argument $argument)
+    {
         return $argument->getName();
     }
 
-    public function getDeclarationLine(Argument $argument){
-        return $this->getStaticTypeOf($argument) . ' ' . $this->getTargetArgumentName($argument).';';
+    public function getDeclarationLine(Argument $argument)
+    {
+        return $this->getStaticTypeOf($argument) . ' ' . $this->getTargetArgumentName($argument) . ';';
     }
 
-    public function getFactoryLine(Argument $argument){
-        switch (true){
+    public function getFactoryLine(Argument $argument)
+    {
+        switch (true) {
             case $argument->is(ArgumentType::TYPE_INTEGER):
-                return $this->getTargetArgumentName($argument).': json["' . $argument->getName() . '"] == null ? null : int.parse(json["' . $argument->getName() . '"].toString()), ';
+                return $this->getTargetArgumentName($argument) . ': json["' . $argument->getName() . '"] == null ? null : int.parse(json["' . $argument->getName() . '"].toString()), ';
 
             case $argument->is(ArgumentType::TYPE_DECIMAL):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null :  double.parse(json["' . $argument->getName() . '"].toString()), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null :  double.parse(json["' . $argument->getName() . '"].toString()), ';
 
             case $argument->is(ArgumentType::TYPE_STRING):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null :  json["' . $argument->getName() . '"].toString(), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null :  json["' . $argument->getName() . '"].toString(), ';
 
             case $argument->is(ArgumentType::TYPE_DATE):
             case $argument->is(ArgumentType::TYPE_DATETIME):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null :  DateTime.parse(json["' . $argument->getName() . '"].toString() ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null :  DateTime.parse(json["' . $argument->getName() . '"].toString() ), ';
 
             case $argument->is(ArgumentType::TYPE_OBJECT):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null :  ' . $argument->getType()->struct->getClassName() . '.fromJson(json["' . $argument->getName() . '"]), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null :  ' . $argument->getType()->struct->getClassName() . '.fromJson(json["' . $argument->getName() . '"]), ';
 
             case $argument->is(ArgumentType::TYPE_LISTOF_INTEGER):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null  : new List<' . $argument->getType()->struct->getClassName() . '>.from ( json["' . $argument->getName() . '"].map( (x)=> int.parse( x.toString() ) ) ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null  : new List<int>.from ( json["' . $argument->getName() . '"].map( (x)=> int.parse( x.toString() ) ) ), ';
 
             case $argument->is(ArgumentType::TYPE_LISTOF_DECIMAL):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null  : new List<' . $argument->getType()->struct->getClassName() . '>.from ( json["' . $argument->getName() . '"].map( (x)=> double.parse( x.toString() ) ) ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null  : new List<decimal>.from ( json["' . $argument->getName() . '"].map( (x)=> double.parse( x.toString() ) ) ), ';
 
             case $argument->is(ArgumentType::TYPE_LISTOF_STRING):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null  : new List<String>.from ( json["' . $argument->getName() . '"].map( (x)=> x.toString() ) ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null  : new List<String>.from ( json["' . $argument->getName() . '"].map( (x)=> x.toString() ) ), ';
 
             case $argument->is(ArgumentType::TYPE_LISTOF_DATE):
             case $argument->is(ArgumentType::TYPE_LISTOF_DATETIME):
-            return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null  : new List<' . $argument->getType()->struct->getClassName() . '>.from ( json["' . $argument->getName() . '"].map( (x)=> DateTime.parse( x.toString() ) ) ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null  : new List<DateTime>.from ( json["' . $argument->getName() . '"].map( (x)=> DateTime.parse( x.toString() ) ) ), ';
 
             case $argument->is(ArgumentType::TYPE_LISTOF_OBJECT):
-                return $this->getTargetArgumentName($argument).':  json["' . $argument->getName() . '"] == null ? null  : new List<' . $argument->getType()->struct->getClassName() . '>.from ( json["' . $argument->getName() . '"].map( (x)=> '.$argument->getType()->struct->getClassName().'.fromJson(x) ) ), ';
+                return $this->getTargetArgumentName($argument) . ':  json["' . $argument->getName() . '"] == null ? null  : new List<' . $argument->getType()->struct->getClassName() . '>.from ( json["' . $argument->getName() . '"].map( (x)=> ' . $argument->getType()->struct->getClassName() . '.fromJson(x) ) ), ';
 
         }
     }
 
-    public function getToJsonLine(Argument $argument){
+    public function getToJsonLine(Argument $argument)
+    {
+        $begin = '"' . $argument->getName() . '": ' . $this->getTargetArgumentName($argument) . ' == null ? null : ';
+        $end = ',';
+        $middle = '';
+        switch (true) {
+            case $argument->is(ArgumentType::TYPE_STRING):
+            case $argument->is(ArgumentType::TYPE_INTEGER):
+            case $argument->is(ArgumentType::TYPE_DECIMAL):
+                $middle = $this->getTargetArgumentName($argument);
+                break;
+            case $argument->is(ArgumentType::TYPE_OBJECT):
+                $middle = $this->getTargetArgumentName($argument) . '.toJson()';
+                break;
 
+            case $argument->is(ArgumentType::TYPE_DATE):
+                $middle = $this->getTargetArgumentName($argument) . '.toIso8601String().substring(0,10)';
+                break;
+
+            case $argument->is(ArgumentType::TYPE_DATETIME):
+                $middle = $this->getTargetArgumentName($argument) . '.toUtc().toString().substring(0,19)';
+                break;
+
+
+            case $argument->is(ArgumentType::TYPE_LISTOF_STRING):
+            case $argument->is(ArgumentType::TYPE_LISTOF_INTEGER):
+            case $argument->is(ArgumentType::TYPE_LISTOF_DECIMAL):
+                $middle = 'new List<dynamic>.from(' . $this->getTargetArgumentName($argument) . '.map((x) => x)) ';
+                break;
+
+            case $argument->is(ArgumentType::TYPE_LISTOF_DATE):
+                $middle = 'new List<dynamic>.from(' . $this->getTargetArgumentName($argument) . '.map((x) => x.toIso8601String().substring(0,10))) ';
+                break;
+
+            case $argument->is(ArgumentType::TYPE_LISTOF_DATETIME):
+                $middle = 'new List<dynamic>.from(' . $this->getTargetArgumentName($argument) . '.map((x) => x.toUtc().toString().substring(0,19))) ';
+                break;
+
+            case $argument->is(ArgumentType::TYPE_LISTOF_OBJECT):
+                $middle = 'new List<dynamic>.from(' . $this->getTargetArgumentName($argument) . '.map((x) => x.toJson())) ';
+                break;
+        }
+        return $begin.$middle.$end;
     }
-    public function getConstructorLine(Argument $argument){
-        return 'this.'.$this->getTargetArgumentName($argument).' ,';
+
+    public function getConstructorLine(Argument $argument)
+    {
+        return 'this.' . $this->getTargetArgumentName($argument) . ' ,';
     }
 
 }
